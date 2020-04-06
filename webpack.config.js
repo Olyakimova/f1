75f6591f795541a5f09e80ef40fac480ea2ebfd2
olyakimova <olyakimova747@gmail.com>
2020-04-06 19:24:07 +0300

    fix build react

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "docs"),
-    publicPath: "/",
+    publicPath: "../",
     filename: "bundle.js",
   },
   devServer: {
